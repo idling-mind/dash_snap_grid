4cb11406ac2ec5a0a227681976835eb851d1dd89
Najeem <najeem@gmail.com>
2024-12-05 19:37:39 +0100

    upgraded package to 0.2.0

--- src/DashSnapGrid.jl
+++ src/DashSnapGrid.jl
@@ -3,7 +3,7 @@ module DashSnapGrid
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.1.0"
+const version = "0.2.0"
 
 include("jl/''_draggablediv.jl")
 include("jl/''_grid.jl")
